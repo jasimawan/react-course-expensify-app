500d6e75074e114164ea9a8e08ed9d751d90d2c3
jasimawan <jasimawan78@gmail.com>
2019-07-31 20:56:20 +0500

    Add Expensify Styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -25,7 +25,6 @@ export default class ExpenseForm extends React.Component {
   };
   onAmountChange = (e) => {
     const amount = e.target.value;
-
     if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
@@ -55,10 +54,10 @@ export default class ExpenseForm extends React.Component {
   };
   render() {
     return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
+        <form className="form" onSubmit={this.onSubmit}>
+          {this.state.error && <p className="form__error">{this.state.error}</p>}
           <input
+            className="text-input"
             type="text"
             placeholder="Description"
             autoFocus
@@ -66,6 +65,7 @@ export default class ExpenseForm extends React.Component {
             onChange={this.onDescriptionChange}
           />
           <input
+            className="text-input"
             type="text"
             placeholder="Amount"
             value={this.state.amount}
@@ -80,14 +80,16 @@ export default class ExpenseForm extends React.Component {
             isOutsideRange={() => false}
           />
           <textarea
+            className="textarea"
             placeholder="Add a note for your expense (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
           >
           </textarea>
-          <button>Add Expense</button>
+          <div>
+              <button className="button">Save Expense</button>
+          </div>
         </form>
-      </div>
     )
   }
 }
